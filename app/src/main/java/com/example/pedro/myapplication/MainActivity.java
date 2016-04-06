4512fa587831d72a00611140250b191f95867be6
helio-luk <helio.ssjr@gmail.com>
2016-04-06 14:07:33 -0300

    Criacao da Tela inicial

--- app/src/main/java/com/example/pedro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pedro/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pedro.myapplication;
 
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
