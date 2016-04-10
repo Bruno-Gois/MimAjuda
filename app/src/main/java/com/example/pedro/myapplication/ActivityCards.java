8f5b9cc4c54a8274ac3840847f298f603beff931
helio-luk <helio.ssjr@gmail.com>
2016-04-10 12:03:07 -0300

    Criação da segunda tela e transição entre elas

---
+++ app/src/main/java/com/example/pedro/myapplication/ActivityCards.java
@@ -0,0 +1,19 @@
+package com.example.pedro.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by helio on 10/04/16.
+ */
+public class ActivityCards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+
+    }
+
+
+}
--- app/src/main/java/com/example/pedro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pedro/myapplication/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.pedro.myapplication;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View btnFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnFacebook = (View) findViewById(R.id.btnFacebook);
+        btnFacebook.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, ActivityCards.class);
+        startActivity(intent);
 
 
     }
