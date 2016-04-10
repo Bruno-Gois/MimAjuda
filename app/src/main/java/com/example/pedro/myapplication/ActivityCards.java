a1d23dbf622ec754d614343ef2da26d655ad8090
helio-luk <helio.ssjr@gmail.com>
2016-04-10 16:50:20 -0300

    Criação do toolbar e menu horizontal

--- app/src/main/java/com/example/pedro/myapplication/ActivityCards.java
+++ app/src/main/java/com/example/pedro/myapplication/ActivityCards.java
@@ -1,19 +1,83 @@
 package com.example.pedro.myapplication;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-/**
- * Created by helio on 10/04/16.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import fragments.OneFragment;
+import fragments.ThreeFragment;
+import fragments.TwoFragment;
+
+
 public class ActivityCards extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //TODO: Trocar o icone do toolbar
+        toolbar.setTitle("               Mim Ajuda");
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Ofertas");
+        adapter.addFragment(new TwoFragment(), "Pedidos");
+        adapter.addFragment(new ThreeFragment(), "Últimos");
+        viewPager.setAdapter(adapter);
+    }
 
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
---
+++ app/src/main/java/fragments/TwoFragment.java
@@ -0,0 +1,31 @@
+package fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pedro.myapplication.R;
+
+/**
+ * Created by helio on 10/04/16.
+ */
+public class TwoFragment extends Fragment {
+
+    public TwoFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        //inflate the layoyt for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+
+    }
+}
